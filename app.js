02713e6af6bd4eed4f132994f7a00ab06fa98135
Rost Khanyukov <rost.khanyukov@gmail.com>
2015-01-27 21:23:49 +0200

    sada

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 var app = angular.module('Monitor', ['ui.bootstrap']);
 
 app.controller('Tabs', function ($scope, $window) {
-  $scope.build1 = function() {
+  $scope.build = function(chartNum) {
     setTimeout(function() { 
-		var data = {
+		var data1 = {
+			labels: ["Школа №5", "Школа №3", "Школа №1", "Школа №2", "Школа №4", "Боромлянська", "Білківська"],
+			datasets: [
+				{
+					label: "My Second dataset",
+					fillColor: "rgba(131,137,255,0.5)",
+					strokeColor: "rgba(0,0,255,0.8)",
+					highlightFill: "rgba(131,137,255,0.75)",
+					highlightStroke: "rgba(131,137,255,1)",
+					data: [2, 1, 0, 0, 0, 0, 0]
+				}
+			]
+		};
+		
+		var data2 = {
 			labels: ["Школа №3", "Школа №5", "Школа №1", "Школа №2", "Школа №4", "Боромлянська", "Білківська"],
 			datasets: [
 				{
@@ -16,8 +30,15 @@ app.controller('Tabs', function ($scope, $window) {
 				}
 			]
 		};
-		var ctx = document.getElementById("myChart").getContext("2d");
-		var myBarChart = new Chart(ctx).Bar(data, {});
+		
+		if (chartNum == 1) {
+			var ctx = document.getElementById("myChart1").getContext("2d");
+			var myBarChart = new Chart(ctx).Bar(data1, {});
+		} 
+		if (chartNum == 2) {
+			var ctx = document.getElementById("myChart2").getContext("2d");
+			var myBarChart = new Chart(ctx).Bar(data2, {});
+		}
     });
   };
 });
