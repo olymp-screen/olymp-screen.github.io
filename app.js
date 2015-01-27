06a26cbbc9fadb443f456e9423eae94fab42726a
Rost Khanyukov <rost.khanyukov@gmail.com>
2015-01-27 22:23:32 +0200

    132

--- app.js
+++ app.js
@@ -31,6 +31,20 @@ app.controller('Tabs', function ($scope, $window) {
 			]
 		};
 		
+		var data3 = {
+			labels: ["Холодько", "Батраченко", "Красніков"],
+			datasets: [
+				{
+					label: "My Second dataset",
+					fillColor: "rgba(131,137,255,0.5)",
+					strokeColor: "rgba(0,0,255,0.8)",
+					highlightFill: "rgba(131,137,255,0.75)",
+					highlightStroke: "rgba(131,137,255,1)",
+					data: [1, 1, 1]
+				}
+			]
+		};
+		
 		if (chartNum == 1) {
 			var ctx = document.getElementById("myChart1").getContext("2d");
 			var myBarChart = new Chart(ctx).Bar(data1, {});
@@ -39,6 +53,18 @@ app.controller('Tabs', function ($scope, $window) {
 			var ctx = document.getElementById("myChart2").getContext("2d");
 			var myBarChart = new Chart(ctx).Bar(data2, {});
 		}
+		if (chartNum == 3) {
+			var ctx = document.getElementById("myChart3").getContext("2d");
+			var myBarChart = new Chart(ctx).Bar(data3, {});
+		}
     });
   };
+});
+
+app.controller('List', function($scope) {	
+	$scope.items = [
+		{ name: 'Холодько Оксана', school: 'СШ №5', subject: 'Інформаційні технології', place: 'third' },
+		{ name: 'Батраченко Владислав', school: 'СШ №5', subject: 'Математика', place: 'third' },
+		{ name: 'Красніков Андрій', school: 'СШ №3', subject: 'Математика', place: 'third' },
+	];
 });
