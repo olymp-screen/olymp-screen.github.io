1042b0e58f1e7535511e8d941794f2d865fc717a
Rost Khanyukov <rost.khanyukov@gmail.com>
2015-02-14 13:04:40 +0200

    Update

--- js/app.js
+++ js/app.js
@@ -6,27 +6,27 @@ function sortTogether(array1, array2) {
 }
 
 var database = {
-	// Результати шкіл в районі
-	inPlaces: [
-		[0, 0, 0, 0, 0, 0], // 1 місця
-		[2, 0, 0, 0, 0, 0], // 2 місця
-		[6, 1, 1, 0, 0, 0] // 3 місця
-	],
-	pPlaces: [
-		[0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[2, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 1, 1, 1, 1, 1, 1, 1, 1]
-	],
-	inRate: [0.857, 0.071, 0.071, 0, 0, 0],
-	inTeams: ["Школа №5", "Школа №3", "Боромлянський НВК", "Школа №2", "Школа №4", "Гребениківська ЗОШ"],
-	
-	regRate: [0.268, 0.161, 0.092, 0.109, 0.052, 0.019, 0.057, 0.025, 0.01, 0.036, 0.029, 0.034, 0.013, 0.023, 0.006, 0.006, 0.004, 0.004, 0.008, 0.006, 0, 0.002, 0.004, 0.002, 0.013, 0.01, 0.004],
-	regTeams: ["м.Суми", "м.Шостка", "м.Конотоп", "Кролевецький район", "Кадетський корпус", "Білопільський район",
-			   "м.Ромни", "Липоводолинський район", "Сумська обл. гімн. для обдар.", "м.Охтирка", "Тростянецький район", 
-			   "Серединобудський район", "Конотопський район", "м.Глухів", "м.Лебедин", "Ямпільський район", 
-			   "Роменський район", "Краснопільський район", "Буринський район", "Великописарівський район", "Сумський район", 
-			   "Глухівський район", "Лебединський район", "Недригайлівський район", "Охтирський район", "Шосткінський район", 
-			   "Путивльський район"]
+    // Результати шкіл в районі
+    inPlaces: [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0], // 1 місця
+        [2, 1, 1, 0, 0, 0, 0, 0, 0], // 2 місця
+        [12, 2, 0, 2, 0, 0, 0, 0, 0] // 3 місця
+    ],
+    pPlaces: [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [2, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 3, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ],
+    inRate: [0.643, 0.179, 0.107, 0.071, 0, 0, 0, 0, 0],
+    inTeams: ["Школа №5", "Школа №3", "Школа №1", "Боромлянський НВК", "Школа №2", "Школа №4", "Гребениківська ЗОШ", "Жигайлівська ЗОШ", "Кам'янська ЗОШ"],
+
+    regRate: [0.304, 0.136, 0.101, 0.088, 0.044, 0.01, 0.051, 0.016, 0.016, 0.03, 0.031, 0.021, 0.014, 0.032, 0.02, 0.005, 0.005, 0.005, 0.008, 0.004, 0, 0.013, 0.009, 0.007, 0.009, 0.009, 0.008],
+    regTeams: ["м.Суми", "м.Шостка", "м.Конотоп", "Кролевецький район", "Кадетський корпус", "Білопільський район",
+        "м.Ромни", "Липоводолинський район", "Сумська обл. гімн. для обдар.", "м.Охтирка", "Тростянецький район",
+        "Серединобудський район", "Конотопський район", "м.Глухів", "м.Лебедин", "Ямпільський район",
+        "Роменський район", "Краснопільський район", "Буринський район", "Великописарівський район", "Сумський район",
+        "Глухівський район", "Лебединський район", "Недригайлівський район", "Охтирський район", "Шосткінський район",
+        "Путивльський район"]
 };
 
 sortTogether(database.regRate, database.regTeams);
@@ -36,90 +36,126 @@ var option1 =  {
     chart: { type: 'bar', animation: 'false' },
     title: { text: '' },
     xAxis: { categories: database.inTeams },
-    yAxis: { title: { text: ''}, max: 10, allowDecimals: false },
+    yAxis: { title: { text: ''}, max: 15, allowDecimals: false },
     plotOptions: { series: { stacking: 'normal' } },
-	series : [
-		{ name: 'І місце',   data: database.inPlaces[0] }, 
-		{ name: 'ІІ місце',  data: database.inPlaces[1] }, 
-		{ name: 'ІІІ місце', data: database.inPlaces[2] }
-	]
+    series : [
+        { name: 'І місце',   data: database.inPlaces[0] },
+        { name: 'ІІ місце',  data: database.inPlaces[1] },
+        { name: 'ІІІ місце', data: database.inPlaces[2] }
+    ]
 };
 
 var option2 =  {
     chart: { type: 'bar' },
     title: { text: '' },
-    xAxis: { categories: ["Школа №5", "Школа №3", "Боромлянський НВК", "Школа №2", "Школа №4"] },
-    yAxis: { title: { text: ''}, max: 1 },
+    xAxis: { categories: database.inTeams },
+    yAxis: { title: { text: ''}, max: 0.8 },
     plotOptions: { series: { stacking: 'normal' } },
-	series : [
-		{ name: 'Рейтинг', data: database.inRate }
-	]
+    series : [
+        { name: 'Рейтинг', data: database.inRate }
+    ]
 };
 
 var option3 =  {
     chart: { type: 'bar' },
     title: { text: '' },
-    xAxis: { categories: ['Куліков', 'Андрущенко', 'Батраченко', 'Кірнос', 'Ковальчук',
-                        'Красніков', 'Тарасенко', 'Трусов', 'Холодько'] },
+    xAxis: { categories: ['Куліков', 'Батраченко', 'Дробітько', 'Протасова', 'Андрущенко', 'Білик', 'Кірнос', 'Ковальчук',
+        'Красніков', 'Рубан', 'Симон', 'Тарасенко', 'Трусов', 'Холодько', 'Хоменко', 'Храмцова'] },
     yAxis: { title: { text: ''}, max: 5, allowDecimals: false },
     plotOptions: { series: { stacking: 'normal' } },
-	series : [
-		{ name: 'І місце',   data: database.pPlaces[0] }, 
-		{ name: 'ІІ місце',  data: database.pPlaces[1] }, 
-		{ name: 'ІІІ місце', data: database.pPlaces[2] }
-	]
+    series : [
+        { name: 'І місце',   data: database.pPlaces[0] },
+        { name: 'ІІ місце',  data: database.pPlaces[1] },
+        { name: 'ІІІ місце', data: database.pPlaces[2] }
+    ]
 };
 
 var option4 =  {
     chart: { type: 'bar' },
     title: { text: '' },
-    xAxis: { 
-		categories: database.regTeams
-	},
+    xAxis: {
+        categories: database.regTeams
+    },
     yAxis: { title: { text: ''}, max: 0.5 },
     plotOptions: { series: { stacking: 'normal' } },
-	series : [
-		{ name: 'Рейтинг', data: database.regRate },
-	]
+    series : [
+        { name: 'Рейтинг', data: database.regRate },
+    ]
 };
 
-var app = angular.module('Monitor', ['ui.bootstrap']);
+
+var app = angular.module('myModule', ['lumx']);
+
+app.controller('List', function($scope) {
+    $scope.schools = [
+        "Тростянецька спеціалізована школа I-III ступенів №1",
+        "Тростянецька спеціалізована школа I-III ступенів №2",
+        "Тростянецька спеціалізована школа I-III ступенів №3",
+        "Тростянецька загальноосвітня школа I-III ступенів №4",
+        "Тростянецька спеціалізована школа I-III ступенів №5",
+        "Боромлянський навчально-виховний комплекс: загальноосвітня школа І-ІІІ ступенів - дошкільний навчальний заклад"
+    ];
+
+    $scope.items = [
+        { id: '1', name: 'Холодько Оксана',      school: $scope.schools[4],
+            subject: 'Інформаційні технології', place: '3' },
+        { id: '2', name: 'Батраченко Владислав', school: $scope.schools[4],
+            subject: 'Математика', place: '3' },
+        { id: '3', name: 'Красніков Андрій',     school: $scope.schools[2],
+            subject: 'Математика', place: '3' },
+        { id: '4', name: 'Ковальчук Юлія',       school: $scope.schools[4],
+            subject: 'Біологія', place: '3'},
+        { id: '5', name: 'Куліков Денис',        school: $scope.schools[4],
+            subject: 'Біологія', place: '2'},
+        { id: '6', name: 'Андрущенко Юлія',      school: $scope.schools[4],
+            subject: 'Біологія', place: '3'},
+        { id: '5', name: 'Куліков Денис',        school: $scope.schools[4],
+            subject: 'Інформатика', place: '2'},
+        { id: '7', name: 'Трусов Богдан',        school: $scope.schools[4],
+            subject: 'Інформатика', place: '3'},
+        { id: '8', name: 'Кірнос Сергій',        school: $scope.schools[5],
+            subject: 'Інформатика', place: '3'},
+        { id: '9', name: 'Тарасенко Світлана',   school: $scope.schools[4],
+            subject: 'Географія', place: '3'},
+        { id: '2', name: 'Батраченко Владислав', school: $scope.schools[4],
+            subject: 'Математика', place: '3'},
+        { id: '10', name: 'Дробітько Дарія',     school: $scope.schools[2],
+            subject: 'Українська мова та література', place: '2'},
+        { id: '11', name: 'Хоменко Владислава',  school: $scope.schools[2],
+            subject: 'Українська мова та література', place: '3'},
+        { id: '12', name: 'Храмцова Юлія',       school: $scope.schools[4],
+            subject: 'Хімія', place: '3'},
+        { id: '2', name: 'Батраченко Владислав', school: $scope.schools[4],
+            subject: 'Хімія', place: '3'},
+        { id: '13', name: 'Рубан Єлизавета',     school: $scope.schools[4],
+            subject: 'Хімія', place: '3'},
+        { id: '6', name: 'Андрущенко Юлія',      school: $scope.schools[4],
+            subject: 'Хімія', place: '3'},
+        { id: '14', name: 'Протасова Альона',    school: $scope.schools[0],
+            subject: 'Історія', place: '2'},
+        { id: '15', name: 'Білик Ганна',         school: $scope.schools[5],
+            subject: 'Історія', place: '3'},
+        { id: '16', name: 'Симон Олексій',       school: $scope.schools[4],
+            subject: 'Англійська мова', place: '3'}
+    ];
+});
 
 app.directive('highcharts', function() {
-	return {
-		link: function(scope, el, attrs) {
-			var options;
-			var chartData = scope.$eval(attrs.chartdata);
-			if (chartData == 1) {
-				options = option1;
-			} else if(chartData == 2) {
-				options = option2;
-			} else if(chartData == 3) {
-				options = option3;
-			} else if(chartData == 4) {
-				options = option4;
-			}
-			options.chart.renderTo = el[0];
-			new Highcharts.Chart(options);
-		}
-	};
-});
-
-app.controller("Tabs", function($scope) {
-
-});
-
-app.controller('List', function($scope) {	
-	$scope.items = [
-		{ name: 'Холодько Оксана', school: 'СШ №5', subject: 'Інформаційні технології', place: 'III' },
-		{ name: 'Батраченко Владислав', school: 'СШ №5', subject: 'Математика', place: 'III' },
-		{ name: 'Красніков Андрій', school: 'СШ №3', subject: 'Математика', place: 'III' },
-        { name: 'Ковальчук Юлія', school: 'СШ №5', subject: 'Біологія', place: 'III'},
-        { name: 'Куліков Денис', school: 'СШ №5', subject: 'Біологія', place: 'II'},
-        { name: 'Андрущенко Юлія', school: 'СШ №5', subject: 'Біологія', place: 'III'},
-        { name: 'Куліков Денис', school: 'СШ №5', subject: 'Інформатика', place: 'II'},
-        { name: 'Трусов Богдан', school: 'СШ №5', subject: 'Інформатика', place: 'III'},
-        { name: 'Кірнос Сергій', school: 'Боромлянський НВК', subject: 'Інформатика', place: 'III'},
-        { name: 'Тарасенко Світлана', school: 'СШ №5', subject: 'Географія', place: 'III'}
-	];
+    return {
+        link: function(scope, el, attrs) {
+            var options;
+            var chartData = scope.$eval(attrs.chartdata);
+            if (chartData == 1) {
+                options = option1;
+            } else if(chartData == 2) {
+                options = option2;
+            } else if(chartData == 3) {
+                options = option3;
+            } else if(chartData == 4) {
+                options = option4;
+            }
+            options.chart.renderTo = el[0];
+            new Highcharts.Chart(options);
+        }
+    };
 });
--- js/theme.js
+++ js/theme.js
@@ -1,6 +1,6 @@
 Highcharts.theme = {
     colors: ['#E1090C', '#3B46E3', '#31A543', '#DDDF00', '#24CBE5', '#64E572', '#FF9655', '#FFF263', '#6AF9C4'],
-    chart: { backgroundColor: '#E6E6E6' },
+    chart: { backgroundColor: '#ffffff' },
     legend: {
         itemStyle: {
             font: '11pt Tahoma, Verdana, sans-serif',
